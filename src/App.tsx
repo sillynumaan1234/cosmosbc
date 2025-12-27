00ebd7525060c66a69a9f5acca2d124109131692
sillynumaan1234 <168008289+sillynumaan1234@users.noreply.github.com>
2025-12-27 11:55:54 +0000

    Fixed issue relating to scroll not resetting

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ScrollToTop from "@/components/ScrollToTop";
+
 import Index from "./pages/Index";
 import Products from "./pages/Products";
 import About from "./pages/About";
@@ -17,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<Products />} />
