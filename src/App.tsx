d2e7595ff0854ac0a478510d6d1c15bd097dee16
sillynumaan1234 <168008289+sillynumaan1234@users.noreply.github.com>
2025-12-27 10:57:01 +0000

    removed dead code

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Index from "./pages/Index";
 import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Showroom from "./pages/Showroom";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -23,7 +22,6 @@ const App = () => (
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/showroom" element={<Showroom />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
